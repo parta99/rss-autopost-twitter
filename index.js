039e3171fc31722583717f6d210d80e2c3fff54e
DE Juniada <dejon933@gmail.com>
2023-03-28 07:12:52 +0800

    report
    report

--- index.js
+++ index.js
@@ -1,41 +1,89 @@
-// Import dependencies
-const Parser = require('rss-parser');
-const Twit = require('twit');
-require('dotenv').config();
+const dotenv = require("dotenv");
+const Twitter = require("twitter");
+const Parser = require("rss-parser");
 
-// Set up Twitter client
-const T = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+dotenv.config();
 
-// Set up RSS parser
+// get RSS feed
 const parser = new Parser();
 
-// Set up function to get and post RSS feed
-async function postFeed() {
-  try {
-    // Parse RSS feed
-    const feed = await parser.parseURL('http://news.google.com/news?hl=en&gl=us&q=bali&um=1&ie=UTF-8&output=rss');
+// Create a Twitter client to manage credentials
+const client = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
 
-    // Loop through items in feed and post to Twitter
-    feed.items.forEach(async (item) => {
-      // Check if item has already been posted
-      const status = await T.get('statuses/user_timeline', { screen_name: '@fsbali88', count: 1 });
-      const alreadyPosted = status.data.some((tweet) => tweet.text === item.title);
+// Generate a random integer, somewhere between 0 and the 'max' value provided
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
-      // Post to Twitter if item has not already been posted
-      if (!alreadyPosted) {
-        const tweet = await T.post('statuses/update', { status: item.title + ' ' + item.link });
-        console.log('Tweet posted: ' + tweet.data.text);
-      }
-    });
-  } catch (err) {
-    console.error(err);
+// Inspired by: https://www.geeksforgeeks.org/get-the-relative-timestamp-difference-between-dates-in-javascript/
+const timeDiff = (prev) => {
+  if (!prev) {
+    return null;
   }
-}
 
-// Call function to post RSS feed on a schedule (every 30 minutes)
-setInterval(postFeed, 30 * 60 * 1000);
+  const curr = new Date();
+  const ms_Min = 60 * 1000; // milliseconds in Minute
+  const ms_Hour = ms_Min * 60; // milliseconds in Hour
+  const ms_Day = ms_Hour * 24; // milliseconds in day
+  const ms_Mon = ms_Day * 30; // milliseconds in Month
+  const ms_Yr = ms_Day * 365; // milliseconds in Year
+  const diff = curr - prev; // difference between dates.
+
+  // If the diff is less then milliseconds in a minute
+  if (diff < ms_Min) {
+    return "A few seconds ago";
+
+    // If the diff is less then milliseconds in a Hour
+  } else if (diff < ms_Hour) {
+    return "A few minutes ago";
+
+    // If the diff is less then milliseconds in a day
+  } else if (diff < ms_Day) {
+    return "A few hours ago";
+
+    // If the diff is less then milliseconds in a Month
+  } else if (diff < ms_Mon) {
+    return "A few days ago";
+
+    // If the diff is less then milliseconds in a year
+  } else if (diff < ms_Yr) {
+    return "A few months ago";
+  } else {
+    return "A few years ago";
+  }
+};
+
+// Using an IIFE to call teh file, automatically run the function, and move on :)
+(async () => {
+  let feed = await parser.parseURL("https://www.antaranews.com/rss/olahraga.xml");
+
+  // Select a post
+  const postNumber = getRandomInt(feed.items.length);
+  const selectedPost = feed.items[postNumber];
+
+  // Determine what the time stamp is
+  const postDate = Date.parse(selectedPost.pubDate);
+  const timeSincePost = timeDiff(postDate);
+  // Generate and send post
+  const postLink = selectedPost.link;
+  const status = `${timeSincePost} I wrote an article called, "${selectedPost.title}". ${postLink}`;
+
+  client.post(
+    "statuses/update",
+    {
+      status: status,
+    },
+    function (error, tweet, response) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(tweet);
+      }
+    }
+  );
+})();
